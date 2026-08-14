1e3a53f56d7d5e55dadc1db57c33444c4072ea30
Calder Lenhart <calderlen@gmail.com>
2026-08-14 19:02:38 -0400

    Repurpose repository for ns-wind

---
+++ First_Sim_RHD_MPI/definitions.h
@@ -0,0 +1,38 @@
+#define  PHYSICS                        RHD
+#define  DIMENSIONS                     1
+#define  GEOMETRY                       SPHERICAL
+#define  BODY_FORCE                     VECTOR
+#define  COOLING                        NO
+#define  RECONSTRUCTION                 LINEAR
+#define  TIME_STEPPING                  RK2
+#define  NTRACER                        0
+#define  PARTICLES                      NO
+#define  USER_DEF_PARAMETERS            5
+
+/* -- physics dependent declarations -- */
+
+#define  EOS                            IDEAL
+#define  ENTROPY_SWITCH                 ALWAYS
+#define  RADIATION                      NO
+
+/* -- user-defined parameters (labels) -- */
+
+#define  M_NS                           0
+#define  GAMMA                          1
+#define  RHO_IN                         2
+#define  CS_REL_0                       3
+#define  V_INIT                         4
+#define  V_INF_INIT                     5
+
+/* so  RHO_IN is fixed density at R_0, CS_REL_0 is the relativistic sound speed at R_0, and V_INIT and V_INF_INIT are the initial condition guesses only, not prescriptions */
+
+/* [Beg] user-defined constants (do not change this line) */
+
+#define  UNIT_DENSITY                   1.0e7
+#define  UNIT_LENGTH                    1.0e5
+#define  H_MASS_FRAC                    0.2
+#define  He_MASS_FRAC                   0.8
+#define  INTERNAL_BOUNDARY              YES
+#define  INTERNAL_BOUNDARY_CFL          NO
+
+/* [End] user-defined constants (do not change this line) */
--- Test_Problems/Doxygen/html/search/files_4.js
+++
@@ -1,4 +0,0 @@
-var searchData=
-[
-  ['limo3_5fstates_2ec_0',['limo3_states.c',['../limo3__states_8c.html',1,'']]]
-];
